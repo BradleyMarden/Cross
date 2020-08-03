9a06ce225622c202c367e53d9890556f1772ebf7
BradleyMarden <bradleymarden@ez-gamelogic.com>
2020-08-03 22:43:28 +0100

    Initial

---
+++ cmake-build-debug/CMakeFiles/Cross.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cross.dir/main.cpp.obj"
+  "Cross.exe"
+  "Cross.exe.manifest"
+  "Cross.pdb"
+  "libCross.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cross.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
